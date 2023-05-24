e0f853ea3d0e68d9fc9153ac09c051868dc4b37d
Sebastian Svensson Thaning <ssth.skp@edu.mercantec.dk>
2023-05-24 13:40:45 +0200

    Added Block,, Board, Game, Keybinds &  Settings classes.

---
+++ AITetris/Classes/Block.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITetris.Classes
+{
+    internal class Block
+    {
+        public Block()
+        {
+            spritePath = "../Assets/Sprits/BlueGreenPrimary.png";
+        }
+        int coordinateX;
+        int coordinateY;
+        string spritePath;
+    }
+}
