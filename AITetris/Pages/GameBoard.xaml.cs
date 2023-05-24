bdf652ad3d46004681d595fd106a472d8448c0fb
ChrisPDev <49097864+ChrisPDev@users.noreply.github.com>
2023-05-24 14:40:54 +0200

    Gameboard UI

---
+++ AITetris/Pages/GameBoard.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AITetris.Pages
+{
+    /// <summary>
+    /// Interaction logic for GameBoard.xaml
+    /// </summary>
+    public partial class GameBoard : Page
+    {
+        public GameBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void GameBoardActionsPauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GameBoardActionsConsumeOneBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GameBoardActionsConsumeTwoBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GameBoardActionsConsumeThreeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
