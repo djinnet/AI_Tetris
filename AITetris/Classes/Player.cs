51ae4e29cf6a1732558dcf6c070df94a98d0ce10
Sebastian Svensson Thaning <ssth.skp@edu.mercantec.dk>
2023-05-24 10:51:58 +0200

    Character and Player classes.

---
+++ AITetris/Classes/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AITetris.Classes
+{
+    internal class Player : Character
+    {
+        public Player(string name) : base(name)
+        {
+            metaCurrency = 0;
+        }
+
+        public int metaCurrency;
+    }
+}
