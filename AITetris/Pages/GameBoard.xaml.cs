8d9d32306c604ef87a9fca381e2c0ed23b9bfaa9
ChrisPDev <49097864+ChrisPDev@users.noreply.github.com>
2023-05-25 11:02:38 +0200

    Timer functions for Start, Stop, Pause, Resume time is done. I left test buttons in the UI and Code behind if we need them again. They are commented out so they wont create problems.

--- AITetris/Pages/GameBoard.xaml.cs
+++ AITetris/Pages/GameBoard.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace AITetris.Pages
 {
@@ -20,10 +22,105 @@ namespace AITetris.Pages
     /// </summary>
     public partial class GameBoard : Page
     {
+        // Timer varibles for the scoreboard timer
+        private DispatcherTimer scoreboardTimer;
+        private DateTime startTime;
+        private TimeSpan elapsedTime;
+        private TimeSpan pausedTime;
+        private bool isScoreboardTimerPaused;
+
         public GameBoard()
         {
             InitializeComponent();
+
+            // Initialize variables
+            isScoreboardTimerPaused = false;
+
+            // Scoreboard timer
+            scoreboardTimer = new DispatcherTimer();
+            StartTime(scoreboardTimer);
         }
+
+        private void StartTime(DispatcherTimer timer)
+        {
+            // Set the interval of the timer in milliseconds
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+
+            // Set the event that happends on tick
+            timer.Tick += Timer_Tick;
+
+            // Save the start time of the timer
+            startTime = DateTime.Now;
+
+            // Start the timer
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Check if the scoreboardtimer is paused
+            if (!isScoreboardTimerPaused)
+            {
+                // Calculate the elapsed time
+                TimeSpan currentTime = DateTime.Now - startTime;
+
+                // Add the calculated time to the elapsed time
+                elapsedTime = currentTime + pausedTime;
+
+                // Update the timer label in the UI
+                if (elapsedTime.TotalMilliseconds >= 0)
+                {
+                    // Custom formatting the timer
+                    string formattedTime = $"{elapsedTime.Hours:D2}:{elapsedTime.Minutes:D2}:{elapsedTime.Seconds:D2}:{elapsedTime.Milliseconds:D3}";
+
+                    // Add the timer to the scoreboard
+                    GameBoardScoreTimeLbl.Content = formattedTime;
+                }
+                else
+                {
+                    // Backup if timer fails the formatting
+                    GameBoardScoreTimeLbl.Content = "00:00:00:000";
+                }
+            }
+        }
+
+        private void StopTime(DispatcherTimer timer)
+        {
+            // Stop the timer
+            timer.Stop();
+
+            // Resetting the elapsed time and the paused time
+            elapsedTime = TimeSpan.Zero;
+            pausedTime = TimeSpan.Zero;
+
+            // Resetting the UI label
+            GameBoardScoreTimeLbl.Content = "00:00:00:000";
+        }
+
+        private void PauseTime(DispatcherTimer timer)
+        {
+            // Change the scoreboardtimer status
+            isScoreboardTimerPaused = true;
+
+            // Stop the timer
+            timer.Stop();
+
+            // Increase the current pause time
+            pausedTime += DateTime.Now - startTime;
+        }
+
+        private void ResumeTime(DispatcherTimer timer)
+        {
+            // Change the scoreboardtimer status
+            isScoreboardTimerPaused = false;
+
+            // Update the starttime to the time of resume
+            startTime = DateTime.Now;
+
+            // Start the timer
+            timer.Start();
+        }
+
 
         private void GameBoardActionsPauseBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -44,5 +141,27 @@ namespace AITetris.Pages
         {
 
         }
+
+        // Test buttons to control the timer
+
+        //private void GameBoardActionsStartTimeTestBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    StartTime(scoreboardTimer);
+        //}
+
+        //private void GameBoardActionsStopTimeTestBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    StopTime(scoreboardTimer);
+        //}
+
+        //private void GameBoardActionsPauseTimeTestBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    PauseTime(scoreboardTimer);
+        //}
+
+        //private void GameBoardActionsResumeTimeTestBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ResumeTime(scoreboardTimer);
+        //}
     }
 }
