1fdc30e7926995cfb0d53389e563c2d8c27d11e2
Sebastian Svensson Thaning <ssth.skp@edu.mercantec.dk>
2023-05-30 14:39:34 +0200

    Made ClearLines work with the new setup, made images smarter.

--- AITetris/Classes/Square.cs
+++ AITetris/Classes/Square.cs
@@ -4,6 +4,9 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
 
 namespace AITetris.Classes
 {
@@ -13,10 +16,17 @@ namespace AITetris.Classes
         {
             this.coordinateX = coordinateX;
             this.coordinateY = coordinateY;
+
             spritePath = "\\Assets\\Sprits\\BlueGreenPrimary.png";
+            fullpath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + spritePath;
+
+            image = new Image();
+            image.Source = new BitmapImage(new Uri(fullpath, UriKind.Absolute));
         }
         public int coordinateX;
         public int coordinateY;
         public string spritePath;
+        public string fullpath;
+        public Image image;
     }
 }
