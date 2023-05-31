ccdde09b9c372635ae4bb46157418ce0cc8da150
ChrisPDev <49097864+ChrisPDev@users.noreply.github.com>
2023-05-31 10:12:44 +0200

    UI for the game over menu

---
+++ AITetris/Controls/GameOverMenu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AITetris.Controllers
+{
+    /// <summary>
+    /// Interaction logic for GameOverMenu.xaml
+    /// </summary>
+    public partial class GameOverMenu : UserControl
+    {
+        public GameOverMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void GameOverMenuControlRevive_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GameOverMenuControlSaveAI_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GameOverMenuControlQuitGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
