951163df2382168617716ff0248c69954e32f6a3
ChrisPDev <49097864+ChrisPDev@users.noreply.github.com>
2023-06-06 13:12:07 +0200

    UserControllers Settings og Leaderboard

---
+++ AITetris/Controls/LeaderboardUserController.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AITetris.Controls
+{
+    /// <summary>
+    /// Interaction logic for LeaderboardUserController.xaml
+    /// </summary>
+    public partial class LeaderboardUserController : UserControl
+    {
+        public LeaderboardUserController()
+        {
+            InitializeComponent();
+
+            FillLeaderboard();
+        }
+        private void FillLeaderboard()
+        {
+            // List<Leaderboard> scores = new List<Leaderboard>();
+
+            // Leaderbordgrid
+            Grid leaderboardGrid = LeaderboardGrid;
+
+            // Amount of rows in the leaderboard
+            int rowCount = leaderboardGrid.RowDefinitions.Count;
+
+            // Amount of columns in the leaderboard
+            int columnCount = leaderboardGrid.ColumnDefinitions.Count;
+
+            // Running for each row in the leaderboard
+            for (int i = 0; i < rowCount; i++)
+            {
+                // Running for each column in the leaderboard
+                for (int j = 0; j < columnCount; j++)
+                {
+                    if (i == 0)
+                    {
+                        // Border for each label
+                        Border border = new Border();
+
+                        // Border styling
+                        border.BorderBrush = Brushes.Silver;
+                        border.BorderThickness = new Thickness(2);
+
+                        // Creating a new label
+                        Label label = new Label();
+
+                        // Add content and styling to the label
+                        switch (j)
+                        {
+                            case 0:
+                                label.Content = "Name";
+                                break;
+                            case 1:
+                                label.Content = "Point";
+                                break;
+                            case 2:
+                                label.Content = "Lines";
+                                break;
+                            case 3:
+                                label.Content = "Time";
+                                break;
+                            case 4:
+                                label.Content = "Character";
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        label.HorizontalAlignment = HorizontalAlignment.Center;
+                        label.VerticalAlignment = VerticalAlignment.Center;
+                        label.FontFamily = new FontFamily("Tahomaa");
+                        label.FontSize = 20;
+                        label.FontWeight = FontWeights.Bold;
+
+                        // Add label to border
+                        border.Child = label;
+
+                        // Add the border to the leaderboardgrid
+                        Grid.SetColumn(border, j);
+                        Grid.SetRow(border, i);
+                        leaderboardGrid.Children.Add(border);
+                    }
+                    else
+                    {
+                        // Border for each label
+                        Border border = new Border();
+
+                        // Border styling
+                        border.BorderBrush = Brushes.Silver;
+                        border.BorderThickness = new Thickness(2);
+
+                        // Creating a new label
+                        Label label = new Label();
+
+                        // Add content and styling to the label
+                        label.Content = "Test: " + i.ToString() + " - " + j.ToString();
+                        label.HorizontalAlignment = HorizontalAlignment.Center;
+                        label.VerticalAlignment = VerticalAlignment.Center;
+                        label.FontFamily = new FontFamily("Tahomaa");
+                        label.FontSize = 15;
+                        label.FontWeight = FontWeights.Bold;
+
+                        // Add label to border
+                        border.Child = label;
+
+                        // Add the border to the leaderboardgrid
+                        Grid.SetColumn(border, j);
+                        Grid.SetRow(border, i);
+                        leaderboardGrid.Children.Add(border);
+                    }
+                }
+            }
+        }
+
+        private void LeaderboardBackToPauseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // NavigationService.Navigate(new Uri("Pages/MainPage.xaml", UriKind.Relative));
+        }
+    }
+}
